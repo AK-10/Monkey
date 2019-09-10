2ae2e79c7cb4991e785f45567a75dbc9d1c308d6
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-09-10 16:52:17 +0900

    impl token

--- Sources/Monkey/main.swift
+++ Sources/Monkey/main.swift
@@ -2,24 +2,3 @@ import Foundation
 
 print("Hello, world!")
 
-enum Token: String {
-    case illegal = "ILLEGAL"
-    case eof = "EOF"
-    
-    case ident = "IDENT" // add, foobar, x, y, ...
-    case int = "INT" // 123456
-    
-    case assign = "="
-    case plus = "+"
-    
-    case comma = ","
-    case semicolon = ";"
-    
-    case lParen = "("
-    case rParen = ")"
-    case lBrace = "{"
-    case rBrace = "}"
-    
-    case function = "FUNCTION"
-    case _let = "LET"
-}
