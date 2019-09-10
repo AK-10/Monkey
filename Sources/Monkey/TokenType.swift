63dbab0b8cba5228ada5edac5ae5a498ac79242b
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-09-10 16:26:32 +0900

    impl token

---
+++ Sources/Monkey/TokenType.swift
@@ -0,0 +1,13 @@
+//
+//  TokenType.swift
+//  Monkey
+//
+//  Created by Atsushi KONISHI on 2019/09/10.
+//
+
+import Foundation
+
+struct TokenType {
+    let type: Token
+    let literal: String
+}
