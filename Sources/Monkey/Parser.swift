caee6e9f152d615c5525cc3f04cdb40913b54632
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-11-13 04:38:55 +0900

    fix lexer bug: invoke readChar after tokenize noteq, eq token

--- Sources/Monkey/Parser.swift
+++ Sources/Monkey/Parser.swift
@@ -11,11 +11,11 @@ typealias PrefixParseFunc = () -> Expression?
 typealias InfixParseFunc = (Expression) -> Expression?
 
 enum EvalPriority: Int, Comparable, Equatable {
-    
+
     static func < (lhs: EvalPriority, rhs: EvalPriority) -> Bool {
         return lhs.rawValue < rhs.rawValue
     }
-    
+
     case lowest
     case equals // ==
     case less_greater // < or >
@@ -23,8 +23,8 @@ enum EvalPriority: Int, Comparable, Equatable {
     case product // *
     case prefix // -(value) or !(value)
     case call // myFunction(x)
-    
-    static func infixOeratorPriority(tokenType: TokenType) -> EvalPriority {
+
+    static func infixOperatorPriority(tokenType: TokenType) -> EvalPriority {
         switch tokenType {
         case .eq, .notEq:
             return .equals
@@ -35,7 +35,7 @@ enum EvalPriority: Int, Comparable, Equatable {
         case .slash, .asterisk:
             return .product
         default:
-            fatalError("\(tokenType) is not infix operator")
+            return .lowest
         }
     }
 }
@@ -44,9 +44,9 @@ struct DummyExpression: Expression {
     var description: String {
         return "dummy expr"
     }
-    
+
     func expressionNode() {}
-    
+
     func tokenLiteral() -> String {
         return "dummy"
     }
@@ -56,23 +56,26 @@ class Parser {
     let lexer: Lexer
     private var currentToken: Token? = nil
     private var peekToken: Token? = nil
-    
+
     var errors: [String] = []
-    
+
     var prefixParseFuncs: [TokenType : PrefixParseFunc] = [:]
     var infixParseFuncs: [TokenType : InfixParseFunc] = [:]
-    
+
     init(_ lexer: Lexer) {
         self.lexer = lexer
         nextToken()
         nextToken()
-        
+
         // identifier
         registerPrefix(tokenType: .ident, fn: parseIdentifier)
 
+        // paren
+        registerPrefix(tokenType: .lParen, fn: parseGroupedExpression)
+
         // integer
         registerPrefix(tokenType: .int, fn: parseIntegerLiteral)
-        
+
         // bool
         registerPrefix(tokenType: ._true, fn: parseBoolLiteral)
         registerPrefix(tokenType: ._false, fn: parseBoolLiteral)
@@ -80,7 +83,7 @@ class Parser {
         // prefix operator
         registerPrefix(tokenType: .minus, fn: parsePrefixExpression)
         registerPrefix(tokenType: .bang, fn: parsePrefixExpression)
-        
+
         // infix operator
         registerInfix(tokenType: .plus, fn: parseInfixExpression)
         registerInfix(tokenType: .minus, fn: parseInfixExpression)
@@ -91,7 +94,7 @@ class Parser {
         registerInfix(tokenType: .lt, fn: parseInfixExpression)
         registerInfix(tokenType: .gt, fn: parseInfixExpression)
     }
-    
+
     func parseProgram() -> Program {
         var statements: [Statement] = []
         while currentToken != nil {
@@ -119,7 +122,7 @@ extension Parser {
             return parseExpressionStatement()
         }
     }
-    
+
     private func parseLetStatement() -> LetStatement? {
         // rootToken: これはletのはず
         guard let rootToken = currentToken else { return nil }
@@ -144,20 +147,20 @@ extension Parser {
             return nil
         }
     }
-    
+
     private func parseReturnStatement() -> ReturnStatement? {
         guard let rootToken = currentToken else { return nil }
-        
+
         nextToken()
-        
+
         // TODO: セミコロンに遭遇するまで式を読み飛ばしている
         while !curTokenIs(tokenType: .semicolon) {
             nextToken()
         }
-        
+
         return ReturnStatement(token: rootToken, returnValue: DummyExpression())
     }
-    
+
     private func parseExpressionStatement() -> ExpressionStatement? {
         guard let rootToken = currentToken else { return nil }
         guard let expr = parseExpression(precedence: .lowest) else { return nil }
@@ -166,10 +169,10 @@ extension Parser {
         while !peekTokenIs(tokenType: .semicolon) {
             nextToken()
         }
-        
+
         return ExpressionStatement(token: rootToken, expr: expr)
     }
-    
+
     private func parseExpression(precedence: EvalPriority) -> Expression? {
         guard let curToken = currentToken else { return nil }
         guard let prefix = prefixParseFuncs[curToken.type] else {
@@ -191,20 +194,20 @@ extension Parser {
             guard let peek = peekToken, let infix = infixParseFuncs[peek.type], let left = leftExpr else {
                 return leftExpr
             }
-            
+
             nextToken()
             leftExpr = infix(left)
         }
-        
+
         return leftExpr
     }
-    
+
     private func parseIdentifier() -> Expression? {
         guard let curToken = currentToken else { return nil }
         print("invoke: parseIdentifier")
         return Identifier(token: curToken, value: curToken.literal)
     }
-    
+
     private func parseIntegerLiteral() -> Expression? {
         guard let curToken = currentToken else { return nil }
         let literal = Int(curToken.literal)
@@ -217,18 +220,25 @@ extension Parser {
             return nil
         }
     }
-    
+
     private func parseBoolLiteral() -> Expression? {
         guard let curToken = currentToken else { return nil }
         let value = curTokenIs(tokenType: ._true)
         return BoolLiteral(token: curToken, value: value)
     }
-    
+
+    private func parseGroupedExpression() -> Expression? {
+        // currentTokenが`(`であるのでその次のトークンからがExpressionのため, nextTokenを実行
+        nextToken()
+        let exp = parseExpression(precedence: .lowest)
+        return expectPeek(tokenType: .rParen) ? exp : nil
+    }
+
     private func parsePrefixExpression() -> Expression? {
         guard let prefixOperatorToken = currentToken else { return nil }
         print("invoke: parsePrefixExpression")
         nextToken()
-        
+
         guard let right = parseExpression(precedence: .prefix) else {
             let msg = "\(prefixOperatorToken.literal) operand is nil"
             errors.append(msg)
@@ -236,17 +246,17 @@ extension Parser {
         }
         return PrefixExpression(token: prefixOperatorToken, op: prefixOperatorToken.literal, right: right)
     }
-    
+
     private func parseInfixExpression(left: Expression) -> Expression? {
         guard let infixOperatorToken = currentToken else { return nil }
         print("invoke: parseInfixExpression")
         let precedence = curPrecedence()
         nextToken()
-        
+
         guard let right = parseExpression(precedence: precedence) else { return nil }
-        
+
         return InfixExpression(token: infixOperatorToken, op: infixOperatorToken.literal, left: left, right: right)
-        
+
     }
 }
 
@@ -256,12 +266,12 @@ extension Parser {
         self.currentToken = peekToken
         peekToken = lexer.nextToken()
     }
-    
+
     private func peekError(tokenType: TokenType) {
         let msg = "expected next token to be \(tokenType.rawValue), got \(peekToken?.type.rawValue ?? "") instead"
         errors.append(msg)
     }
-    
+
     private func expectPeek(tokenType: TokenType) -> Bool {
         if peekTokenIs(tokenType: tokenType) {
             nextToken()
@@ -269,17 +279,17 @@ extension Parser {
         }
         return false
     }
-    
+
     private func curTokenIs(tokenType: TokenType) -> Bool {
         guard let curToken = currentToken else { return false }
         return curToken.type == tokenType
     }
-    
+
     private func peekTokenIs(tokenType: TokenType) -> Bool {
         guard let peekToken = peekToken else { return false }
         return peekToken.type == tokenType
     }
-    
+
     // semantic codeを追加するための関数(prefix)
     private func registerPrefix(tokenType: TokenType, fn: @escaping PrefixParseFunc) {
         prefixParseFuncs[tokenType] = fn
@@ -289,25 +299,25 @@ extension Parser {
     private func registerInfix(tokenType: TokenType, fn: @escaping InfixParseFunc) {
         infixParseFuncs[tokenType] = fn
     }
-    
+
     private func noPrefixParseFuncError(tokenType: TokenType) {
         let msg = "no prefix parse function for \(tokenType.rawValue) found"
         errors.append(msg)
     }
-    
+
     private func peekPrecedence() -> EvalPriority {
         guard let peek = peekToken else {
 //            fatalError("Parser: peekToken is nil")
             return .lowest
         }
-        return EvalPriority.infixOeratorPriority(tokenType: peek.type)
+        return EvalPriority.infixOperatorPriority(tokenType: peek.type)
     }
-    
+
     private func curPrecedence() -> EvalPriority {
         guard let current = currentToken else {
 //            fatalError("Parser: currentToken is nil")
             return .lowest
         }
-        return EvalPriority.infixOeratorPriority(tokenType: current.type)
+        return EvalPriority.infixOperatorPriority(tokenType: current.type)
     }
 }
