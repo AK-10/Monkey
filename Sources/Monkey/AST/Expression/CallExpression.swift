62b00ea526372327ad276ae7727ac19b1e144b84
Atsushi KONISHI <atsushi524k10@gmail.com>
2019-11-18 00:28:41 +0900

    impl parseCallExpression

---
+++ Sources/Monkey/AST/Expression/CallExpression.swift
@@ -0,0 +1,27 @@
+//
+//  CallExpression.swift
+//  Monkey
+//
+//  Created by Atsushi KONISHI on 2019/11/17.
+//
+
+import Foundation
+
+struct CallExpression: Expression {
+    let token: Token
+    let function: Expression
+    let arguments: [Expression]
+    
+    var description: String {
+        let args = ["("] + arguments.map { $0.description } + [")"]
+        return ([function.description] + args).reduce("") { $0 + $1 + "\n" }
+    }
+    
+    func expressionNode() {
+        
+    }
+    
+    func tokenLiteral() -> String {
+        return token.literal
+    }
+}
