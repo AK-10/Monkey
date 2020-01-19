39206f43b4516c6c44dfb869b1b8a1df526c4c15
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-01-19 09:23:13 +0900

    pull eval branch

---
+++ Sources/Monkey/Object/Function.swift
@@ -0,0 +1,18 @@
+//
+//  Function.swift
+//  Monkey
+//
+//  Created by Atsushi KONISHI on 2020/01/19.
+//
+
+import Foundation
+
+struct Function: Object {
+    
+    
+    func type() -> ObjectType {
+    }
+    
+    func inspect() -> String {
+    }
+}
