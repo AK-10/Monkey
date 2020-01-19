a3d20ccfa1d1f2154c95b19ea8ff6b1b3ed93f94
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-01-19 09:46:05 +0900

    impl function object

--- Sources/Monkey/Object/Function.swift
+++ Sources/Monkey/Object/Function.swift
@@ -8,11 +8,17 @@
 import Foundation
 
 struct Function: Object {
-    
+    let parameters: [Identifier]
+    let body: BlockStatement
+    let env: Environment
     
     func type() -> ObjectType {
+        return .function
     }
     
     func inspect() -> String {
+        let paramsStr = parameters.map { $0.description }.joined(separator: ", ")
+        let out = ["fn", "(", paramsStr, ")", "{", body.description, "}"]
+        return out.joined(separator: "\n")
     }
 }
